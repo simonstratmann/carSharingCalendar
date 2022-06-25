ca8af6daa3f093975b55c185e22797a4b6ccafe8
Simon Stratmann <stratmann.simon@gmail.com>
2022-06-25 13:57:37 +0200

    Einfaches BasicAuth

---
+++ src/main/java/de/sist/csc/web/base/Security.java
@@ -0,0 +1,64 @@
+// (C) 2022 PPI AG
+package de.sist.csc.web.base;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class Security extends WebSecurityConfigurerAdapter {
+
+    private static final int SECONDS_PER_DAY = 60 * 60 * 24;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests().anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .rememberMe()
+                .alwaysRemember(true)
+                .key("csc")
+                .tokenValiditySeconds(100 * SECONDS_PER_DAY);
+    }
+
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+
+        return new InMemoryUserDetailsManager(
+                User.builder()
+                        .username("kuni")
+                        .password("marienloh")
+                        .roles("USER")
+                        .build(),
+                User.builder()
+                        .username("hilde")
+                        .password("marienloh")
+                        .roles("USER")
+                        .build()
+                , User.builder()
+                .username("marianne")
+                .password("marienloh")
+                .roles("USER")
+                .build()
+        );
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
